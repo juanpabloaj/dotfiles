061c9287c176c6b650b80de54cbe64b9cb1654a8
JuanPablo <jpabloaj@gmail.com>
2017-01-03 15:21:55 -0300

    tmux launcher with $W for wiki

--- .tmux/tmux_launcher.sh
+++ .tmux/tmux_launcher.sh
@@ -5,6 +5,7 @@ echo "    p ) tmux split-window 'ipython' "
 echo "    q ) quit launcher"
 echo "    s ) tmux split-window 'vim +VimShell' "
 echo "    S ) tmux new-window 'vim +VimShell' "
+echo "    w ) tmux split-window 'nvim $W'"
 echo "    z ) tmux split-window 'zsh'"
 echo "    * ) tmux new-window 'vim' "
 read -n 1 input
@@ -13,6 +14,7 @@ case $input in
     s ) tmux split-window -p 100 'vim +VimShell' ;;
     S ) tmux new-window 'vim +VimShell' ;;
     q ) ;;
+    w ) tmux split-window -p 100 'nvim $W' ;;
     z ) tmux split-window -p 100 'zsh' ;;
     * ) tmux new-window "vim" ;;
 esac
