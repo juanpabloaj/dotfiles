9e05b822f7b20e508972be395e16ba8ce5ca6cd3
JuanPablo <jpabloaj@gmail.com>
2023-07-13 21:12:26 -0400

    nvim, init.lua, treesitter only with elixir

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -16,7 +16,7 @@ require("nvim-tree").setup()
 vim.keymap.set("n", "<leader>n", ':NvimTreeToggle<cr>', { desc = "open nvim tree" })
 
 require'nvim-treesitter.configs'.setup{
-  ensure_installed = { "vim", "lua", "elixir", "heex" },
+  ensure_installed = { "elixir", "heex" },
   -- to install another language manually :TSInstall supported_language
 
   highlight={
