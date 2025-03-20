18d325341e02795d4e65862a2428b8a92318e8f3
JuanPablo <jpabloaj@gmail.com>
2025-03-20 13:00:35 -0300

    vim, updated vim spell files url

--- .vim/spell/spell.sh
+++ .vim/spell/spell.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ## para descargar diccionario de ortografia
-cd $HOME/.vim/spell
+
+[ ! -d $HOME/.vim/spell ] && echo "directory $HOME/.vim/spell not found" && exit 1
+
+pushd $HOME/.vim/spell
+
 leng="es en"
 for l in $leng
 do
@@ -9,7 +13,7 @@ do
 	if [ ! -e $l0 ]
 	then
 		echo "Descargando $l0"
-		wget -c http://ftp.vim.org/pub/vim/runtime/spell/$l0
+		wget -c https://ftp.nluug.nl/pub/vim/runtime/spell/$l0
 	else 
 		echo "Ya existe $l0"
 	fi 
@@ -17,10 +21,12 @@ do
 	if [ ! -e $l1 ]
 	then
 		echo "Descargando $l1"
-		wget -c http://ftp.vim.org/pub/vim/runtime/spell/$l1
+		wget -c https://ftp.nluug.nl/pub/vim/runtime/spell/$l1
 	else
 		echo "Ya existe $l1"
 	fi 
 done
 #wget -c http://ftp.vim.org/pub/vim/runtime/spell/es.utf-8.sug
 #wget -c http://ftp.vim.org/pub/vim/runtime/spell/es.utf-8.spl
+
+popd
